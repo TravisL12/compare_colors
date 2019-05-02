f7dbd81a69cfd6edf3eb04a76d561ea7337a3781
Travis Lawrence <travis.lawrence@uber.com>
2019-05-02 10:39:36 -0700

    Updates

---
+++ precache-manifest.8f7b19e47c12ee9fdd04e2103196bf25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a306399d6706dc70998b943c302de6",
+    "url": "/compare_colors/index.html"
+  },
+  {
+    "revision": "8538a04321d089044980",
+    "url": "/compare_colors/static/css/main.98221492.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/compare_colors/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8538a04321d089044980",
+    "url": "/compare_colors/static/js/main.1e3db3d4.chunk.js"
+  },
+  {
+    "revision": "2d763bb446589fe81f42",
+    "url": "/compare_colors/static/js/runtime~main.1cccd3f4.js"
+  }
+]);
