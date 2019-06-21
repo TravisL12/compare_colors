df1988fba1d2fcd2d2ad50bbaeb9449e557f59cf
Travis Lawrence <travis.lawrence@uber.com>
2019-06-21 12:23:50 -0700

    Updates

--- precache-manifest.d5d04e6c7a09e3f1337a8008dfe5f5e7.js
+++ precache-manifest.d5d04e6c7a09e3f1337a8008dfe5f5e7.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e88528f20a20f08e03161c5f76dad52",
+    "revision": "3f1eaab66cfabbdcf563617f83dbbc7b",
     "url": "/compare_colors/index.html"
   },
   {
-    "revision": "211770a96e783ea56c79",
-    "url": "/compare_colors/static/css/main.79a53b02.chunk.css"
+    "revision": "05d7c3b38dc759acf119",
+    "url": "/compare_colors/static/css/main.07b41b35.chunk.css"
   },
   {
     "revision": "e9a36157479424ac9978",
     "url": "/compare_colors/static/js/2.e8f5b916.chunk.js"
   },
   {
-    "revision": "211770a96e783ea56c79",
+    "revision": "05d7c3b38dc759acf119",
     "url": "/compare_colors/static/js/main.a9dae685.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/compare_colors/precache-manifest.f655e1bb0bf3301ae5ea1dbb8697a3f9.js"
+  "/compare_colors/precache-manifest.d5d04e6c7a09e3f1337a8008dfe5f5e7.js"
 );
 
 self.addEventListener('message', (event) => {
