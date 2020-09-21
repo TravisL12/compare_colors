b71d00ce64333687ee42e2b0e98de3d87e45d7e1
Travis Lawrence <travis@instabase.com>
2020-09-21 16:09:20 -0700

    Updates

--- precache-manifest.fdc6b4bfae3cceae38ff34fd61362b5e.js
+++ precache-manifest.fdc6b4bfae3cceae38ff34fd61362b5e.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1d577d8574b3ed8e95a39750c463b39b",
+    "revision": "28b8114a7f1741a98fc96d256d680eeb",
     "url": "/compare_colors/index.html"
   },
   {
-    "revision": "75ef469995f0628d2d00",
-    "url": "/compare_colors/static/css/main.133609c3.chunk.css"
+    "revision": "96131cf9f087dbb8dc93",
+    "url": "/compare_colors/static/css/main.eb6e920e.chunk.css"
   },
   {
     "revision": "f1e870147a763a3967c2",
     "url": "/compare_colors/static/js/2.086b7c8d.chunk.js"
   },
   {
-    "revision": "75ef469995f0628d2d00",
-    "url": "/compare_colors/static/js/main.72c1f0f7.chunk.js"
+    "revision": "96131cf9f087dbb8dc93",
+    "url": "/compare_colors/static/js/main.8957a368.chunk.js"
   },
   {
     "revision": "2d763bb446589fe81f42",
