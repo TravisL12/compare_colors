12fc60cd82e51f1eea1a1be2abd645bb2455892d
Travis Lawrence <travis.lawrence12@gmail.com>
2021-09-05 21:00:15 -0700

    Updates

--- precache-manifest.d2fe4412664b740900951ef1b45d4e10.js
+++ precache-manifest.d2fe4412664b740900951ef1b45d4e10.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a02588fae9ee7317d1f8248e8f87af30",
+    "revision": "bac7351a5ce2c77d75a578df81151819",
     "url": "/compare_colors/index.html"
   },
   {
-    "revision": "b3cb1be33d6cfb83e1eb",
-    "url": "/compare_colors/static/css/main.ef9f0a18.chunk.css"
+    "revision": "62ea7684fb6d32e36e1f",
+    "url": "/compare_colors/static/css/main.a54eeef7.chunk.css"
   },
   {
     "revision": "1d1d4b292265e65850c2",
     "url": "/compare_colors/static/js/2.922b1c47.chunk.js"
   },
   {
-    "revision": "b3cb1be33d6cfb83e1eb",
-    "url": "/compare_colors/static/js/main.39da0084.chunk.js"
+    "revision": "62ea7684fb6d32e36e1f",
+    "url": "/compare_colors/static/js/main.dbe45ce9.chunk.js"
   },
   {
     "revision": "2d763bb446589fe81f42",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/compare_colors/precache-manifest.3c0193d75c39887c1d87325cf5ad4a75.js"
+  "/compare_colors/precache-manifest.d2fe4412664b740900951ef1b45d4e10.js"
 );
 
 self.addEventListener('message', (event) => {
