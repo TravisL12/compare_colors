90596c9121bee59ab2a81592250ac3df66c76992
Travis Lawrence <travis@instabase.com>
2021-09-08 14:22:26 -0700

    it begins

--- src/components/GridControls.js
+++ src/components/GridControls.js
@@ -1,5 +1,33 @@
 import React from "react";
 import { SORT_OFF, SORT_CHROMATIC, SORT_DELTA } from "../constants";
+import styled from "styled-components";
+
+const SGridControl = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap:
+  width: 100%;
+`;
+
+const SColorInputOptions = styled.div`
+  input[type="color"] {
+    height: 40px;
+    width: 40px;
+    padding: 0;
+    margin: 0;
+    background: transparent;
+  }
+
+  input[type="text"] {
+    width: 60px;
+    text-align: center;
+  }
+`;
+
+const SOptions = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 function GridControls({
   areColorsSorted,
@@ -10,8 +38,8 @@ function GridControls({
   updateCompareColor,
 }) {
   return (
-    <div className="options-container">
-      <div className="options compare-color-options">
+    <SGridControl>
+      <SColorInputOptions>
         <input
           type="color"
           id="compare-color-type"
@@ -25,47 +53,49 @@ function GridControls({
           value={compareColor.name || compareColor.hexColor}
           placeholder="#000000 (Default)"
         />
-      </div>
+      </SColorInputOptions>
 
-      <div className="options sort-info-options">
-        <button className={showInfo ? "on" : "off"} onClick={toggle.info}>
-          {showInfo ? "Details On" : "Details Off"}
-        </button>
-      </div>
+      <SOptions>
+        <div>
+          <button className={showInfo ? "on" : "off"} onClick={toggle.info}>
+            {showInfo ? "Details On" : "Details Off"}
+          </button>
+        </div>
 
-      <div className="options distance-options">
-        <input
-          type="radio"
-          id="compare-off"
-          name="sortOption"
-          checked={sortMethod === "off"}
-          onChange={toggle.sort}
-          value={SORT_OFF}
-          disabled={!areColorsSorted}
-        />
-        <label htmlFor="compare-off">Sort Off</label>
+        <div>
+          <input
+            type="radio"
+            id="compare-off"
+            name="sortOption"
+            checked={sortMethod === "off"}
+            onChange={toggle.sort}
+            value={SORT_OFF}
+            disabled={!areColorsSorted}
+          />
+          <label htmlFor="compare-off">Sort Off</label>
 
-        <input
-          type="radio"
-          id="compare-distanceChromatic"
-          name="sortOption"
-          checked={sortMethod === "distanceChromatic"}
-          onChange={toggle.sort}
-          value={SORT_CHROMATIC}
-        />
-        <label htmlFor="compare-distanceChromatic">Chromatic Sort</label>
+          <input
+            type="radio"
+            id="compare-distanceChromatic"
+            name="sortOption"
+            checked={sortMethod === "distanceChromatic"}
+            onChange={toggle.sort}
+            value={SORT_CHROMATIC}
+          />
+          <label htmlFor="compare-distanceChromatic">Chromatic Sort</label>
 
-        <input
-          type="radio"
-          id="compare-distanceDelta"
-          name="sortOption"
-          checked={sortMethod === "distanceDelta"}
-          onChange={toggle.sort}
-          value={SORT_DELTA}
-        />
-        <label htmlFor="compare-distanceDelta">DeltaE Sort</label>
-      </div>
-    </div>
+          <input
+            type="radio"
+            id="compare-distanceDelta"
+            name="sortOption"
+            checked={sortMethod === "distanceDelta"}
+            onChange={toggle.sort}
+            value={SORT_DELTA}
+          />
+          <label htmlFor="compare-distanceDelta">DeltaE Sort</label>
+        </div>
+      </SOptions>
+    </SGridControl>
   );
 }
 
